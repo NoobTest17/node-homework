7ac4c46a2699720238cc1387169cd1cf894093bf
NoobTest17 <maximnnn.nnn@yandex.ru>
2023-02-12 20:59:08 +0300

    test

--- index.js
+++ index.js
@@ -5,8 +5,48 @@ const isAvailability = (fileName) => {
   return fs.existsSync(fileName)
 }
 
-const createObjectDir = (dir) => {
-  
+const readDirFunct = (pathDir, dir) => {
+  const obj = {
+    name: dir,
+    type: 'dir',
+    content: []
+  }
+
+  const readDir = fs.readdirSync(path.join(pathDir, dir));
+  for (const value of readDir) {
+    if (path.extname(value)) {
+      console.log('fail --->' + value);
+    } else {
+      console.log('dir --->' + value)
+    }
+  }
+
+  return obj
+}
+
+const createObjectDir = (pathDir) => {
+  const dir = {
+    MyDir: {
+      name: path,
+      type: 'dir',
+      content: []
+    }
+  };
+
+  let filesCount = 0;
+  let dirCount = 0;
+  const readDir = fs.readdirSync(pathDir)
+  for (const value of readDir) {
+    if (path.extname(value)) {
+      dirCount++;
+      dir.MyDir.content.push(readDirFunct(readDir, value))
+      console.log('fail --->' + value);
+    } else {
+      filesCount++;
+      console.log('dir --->' + value)
+    }
+  }
+
 }
 
 const program = () => {
@@ -15,11 +55,13 @@ const program = () => {
 
   const checkFile = args.indexOf('-checkfile');
   if (checkFile !== -1) {
-    const isCheckFile = isAvailability(args[checkFile + 1]);
-    return {}
+    return isAvailability(args[checkFile + 1]);
   }
 
-  console.log(args)
+  const readObjDir = args.indexOf('-readdir');
+  if (readObjDir !== -1) {
+    return createObjectDir(args[readObjDir + 1]);
+  }
 }
 
-program();
+console.log(program());
